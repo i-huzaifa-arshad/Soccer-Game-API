055b936bdee0942075b9be928e0e44c4f62d2a9f
i-huzaifa-arshad <huzaifaarshad@hotmail.com>
2024-01-22 19:33:08 +0500

    Modified Login View

--- api/views.py
+++ api/views.py
@@ -32,14 +32,9 @@ class UserLoginView(generics.GenericAPIView):
                 token = Token.objects.create(user=user)
                 team = Team.objects.get(owner=user)
                 team_data = TeamSerializer(team).data
-                # return Response({
-                #     'message': f'Welcome {user.name} to the Soccer Game Console. Here is your Team {team} details',
-                #    # 'token': token.key, # Hiding the token for now
-                #     'team': team_data
-                # })
                 return Response({
-                    'status': 'Successfuly logged in!',
-                    'message': f'Welcome {user.name} to the Soccer Game Console.'
+                    'message': f'Welcome *{user.name}* to the Soccer Online Game Manager Console. Your Team *{team}* is waiting for you!',
+                   # 'token': token.key # Hiding the token for now
                 })
         else:
             return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
