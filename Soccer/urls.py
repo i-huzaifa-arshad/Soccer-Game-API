a0a4c9ed6edd9ae62c9a5db4559feeca20e43b67
i-huzaifa-arshad <huzaifaarshad@hotmail.com>
2024-02-06 16:42:45 +0500

    Updated Swagger path

--- Soccer/urls.py
+++ Soccer/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path(
-        "swagger/",
+        "",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
